4bfbcd4f5180560ba3b13e73ae6c313fc8b94d13
Rian Brunelli <rian10brunelli@gmail.com>
2021-07-16 15:45:28 -0300

    Html e css implementado

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        name: 'Fulano de Tal',
+        partido: 'ABC',
+        fotos:[
+          {url:'38111.png', legenda: 'Vereador'}
+        ]
+      },
+      {
+        numero: 77222,
+        name: 'Beltrano da Silva',
+        partido: 'DEGF',
+        fotos:[
+          {url:'77222.png', legenda: 'Vereador'}
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'Prefeito',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        nome: 'Ciclano',
+        partido: 'ABC',
+        vice: 'CISC',
+        fotos:[
+          {url:'99.png', legenda: 'Prefeito'},
+          {url:'99_2.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: 84,
+        nome: 'Zulano',
+        partido: 'QWERTY',
+        vice: 'ZUL',
+        fotos:[
+          {url:'84.png', legenda: 'Prefeito'},
+          {url:'84_2.png', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+    ]
+  }
+];
---
+++ script.js
@@ -0,0 +1,10 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4 span');
+let aviso = document.querySelector('.d-2');
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+function clicou(n){
+  alert("Clicou em "+n);
+}
